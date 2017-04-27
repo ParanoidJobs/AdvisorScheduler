feb0c10abaf8bf0dc2c3cdbb5287ade6b69e28bc
Samuel Zamudio <szamudio15@hotmail.com>
2017-04-27 14:24:33 -0700

    added instructor home page and screen to edit schedule

---
+++ Advisor-Scheduler/InstructorNVC.swift
@@ -0,0 +1,33 @@
+//
+//  InstructorNVC.swift
+//  Advisor-Scheduler
+//
+//  Created by Samuel Zamudio on 4/26/17.
+//  Copyright © 2017 Paranoid-Jobs. All rights reserved.
+//
+
+import UIKit
+
+class InstructorNVC: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//         Get the new view controller using segue.destinationViewController.
+//         Pass the selected object to the new view controller.
+        
+    }
+}
